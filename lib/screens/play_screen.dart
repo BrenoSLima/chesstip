1502cb614bae34105f0411d84552e69a643ef5d1
Breno Lima <brenobreno1949@gmail.com>
2024-04-04 10:48:14 -0300

    criação do component jogar, um menu com configurações para procurar partida

---
+++ lib/screens/play_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import '../components/play_component.dart';
+
+class PlayScreen extends StatefulWidget {
+  const PlayScreen({Key? key}) : super(key: key);
+
+  @override
+  State<PlayScreen> createState() => _PlayScreenState();
+}
+
+class _PlayScreenState extends State<PlayScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          SizedBox(height: 50,),
+          PlayComponent(),
+        ],
+      ),
+    );
+  }
+}
