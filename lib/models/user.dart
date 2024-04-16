40744276b5af17441c0aaf44e0920d3029aaa58e
Leonardo Alves da Silva <leoal_ves@hotmail.com>
2024-04-16 20:55:04 -0300

    Alteração da tela Login

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,11 +1,16 @@
 class User {
   int id;
   String name;
+  String email;
   double balance;
   String avatar = "assets/images/default-avatar.png";
+  String senha;
+
   User({
     required this.id,
     required this.name,
+    required this.email,
     required this.balance,
+    required this.senha,
   });
 }
--- lib/repositories/user_repository.dart
+++ lib/repositories/user_repository.dart
@@ -1,17 +1,32 @@
 import 'package:chesstip/models/user.dart';
 
 class UserRepository {
-  static User user = User(id: 1, name: "Fabiano Caruana", balance: 100.00);
+  static User user = User(
+      id: 1,
+      name: "Fabiano Caruana",
+      email: 'teste@teste.com',
+      balance: 100.00,
+      senha: '123');
 
   static List<User> users = [
-    User(id: 1, name: "Leonardo", balance: 100.00),
-    User(id: 2, name: "Luis", balance: 100.00),
-    User(id: 3, name: "Mauro", balance: 100.00),
+    User(
+        id: 1,
+        name: "Leonardo",
+        email: 'teste',
+        balance: 100.00,
+        senha: '1234'),
+    User(id: 2, name: "Luis", email: 'teste', balance: 100.00, senha: '1234'),
+    User(id: 3, name: "Mauro", email: 'teste', balance: 100.00, senha: '1234'),
   ];
 
   static List<User> friends = [
-    User(id: 1, name: "Leonardo", balance: 100.00),
-    User(id: 2, name: "Luis", balance: 100.00),
-    User(id: 3, name: "Mauro", balance: 100.00),
+    User(
+        id: 1,
+        name: "Leonardo",
+        email: 'teste',
+        balance: 100.00,
+        senha: '1234'),
+    User(id: 2, name: "Luis", email: 'teste', balance: 100.00, senha: '1234'),
+    User(id: 3, name: "Mauro", email: 'teste', balance: 100.00, senha: '1234'),
   ];
 }
